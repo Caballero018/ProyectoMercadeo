e78644619395d1bfecacf633b473e036936c3c84
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-27 17:51:17 -0500

    E-mail validation

---
+++ app/Http/Controllers/VerificationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class VerificationController extends Controller
+{
+    public function verify($id, $hash)
+    {
+        $user = User::findOrFail($id);
+
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->to('/');
+        }
+
+        if ($user->markEmailAsVerified()) {
+            $user->save;
+            event(new \Illuminate\Auth\Events\Verified($user));
+        }
+
+        return redirect()->to('/');
+    }
+}
