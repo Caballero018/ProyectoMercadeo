f0728bd2d4a7f1c3046bf7b04064b1b2944a660b
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-28 11:42:30 -0500

    Update users view

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -3,13 +3,15 @@
 @section('content')
     <main>
         @forelse ($users as $user)
-            <div style="padding: 20px; margin: 10px;">
-                <h2>{{$user->username}}</h2>
-                <p>{{$user->name}} {{$user->lastname ?? ''}}</p>
-                <p>{{$user->gender}}</p>
-                <p>{{$user->email}}</p>
-                <a href="{{route('admin.users.edit', $user->id)}}">User edit</a>
-            </div>
+            @if ($user->username !== auth()->user()->username)
+                <div style="padding: 20px; margin: 10px;">
+                    <h2>{{$user->username}}</h2>
+                    <p>{{$user->name}} {{$user->lastname ?? ''}}</p>
+                    <p>{{$user->gender}}</p>
+                    <p>{{$user->email}}</p>
+                    <a href="{{route('admin.users.edit', $user->id)}}">User edit</a>
+                </div>
+            @endif
         @empty
             <h2>No existe ningun usuario</h2>
         @endforelse
