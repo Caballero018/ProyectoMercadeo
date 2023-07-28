6c517e7cee506b50e5c3c1422cd1155473e10457
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-28 11:41:52 -0500

    Add User Edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,15 +1,16 @@
 <h1>Editar usuario</h1>
 <p>{{$user->name}}</p>
-<form action="#" method="post">
+<form action="" method="POST">
+    @method('PUT')
     @csrf
     <label for="">Name: </label>
-    <input type="text" placeholder="{{$user->name}}">
+    <input type="text" name="name" placeholder="{{$user->name}}">
 
     <label for="">Last Name: </label>
     @if(empty($user->lastname))
-        <input type="text" placeholder="No last name">
+        <input type="text" name="lastname" placeholder="No last name">
     @else
-        <input type="text" placeholder="{{$user->lastname}}">
+        <input type="text" name="lastname" placeholder="{{$user->lastname}}">
     @endif
 
     <?php
@@ -36,8 +37,20 @@
     @endforeach
 
     <label for="">User Name: </label>
-    <input type="text" placeholder="{{$user->username}}">
+    <input type="text" name="username" placeholder="{{$user->username}}">
+    @error('username')
+        <p>* {{$message}}</p>
+    @enderror
 
     <label for="">Email: </label>
-    <input type="email" placeholder="{{$user->email}}">
+    <input type="email" name="email" placeholder="{{$user->email}}">
+    @error('email')
+        <p>* {{$message}}</p>
+    @enderror
+
+    <Label>Disabled: </Label>
+    <input type="hidden" name="disabled" value="0">
+    <input type="checkbox" name="disabled" value="1" {{ $user->disabled ? 'checked' : '' }}>
+
+    <input type="submit" value="Send">
 </form>
