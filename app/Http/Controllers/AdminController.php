1020cb6d17bc9a23b668437f58791a4015b5978b
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-28 17:09:58 -0500

    Documentation of AdminController controller functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,19 +8,38 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    /**
+     * Function that shows the main view of the Admin
+    */
     public function index()
     {
         return view('admin.index');
     }
+
+    /**
+     * Function that shows view of all users
+     */
     public function usersShow()
     {
         $users = User::all();
         return view('admin.users.users', compact('users'));
     }
+
+    /**
+     * Function that shows view of all users
+     * 
+     * @param App\Models\User $user
+     */
     public function userEdit(User $user)
     {
         return view('admin.users.edit', compact('user'));
     }
+    /**
+     * Function edited by a specific user
+     * 
+     * @param App\Http\Requests\AdminRequest $request
+     * @param App\Models\User $user
+     */
     public function edit(AdminRequest $request, User $user)
     {
         $data = array_filter($request->all(), function ($value) {
